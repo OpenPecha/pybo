1f6c9c07d087fe0233237b75231241e0f6904db1
drupchen <hhdrupchen@gmail.com>
2019-11-07 22:41:26 +0100

    change to "pybo tok", externalize all files in Documents/pybo

--- pybo/cli.py
+++ pybo/cli.py
@@ -1,8 +1,47 @@
-import click
 from pathlib import Path
+from shutil import rmtree
 
-from botok import Text, __version__, rdr_2_replace_matcher
+import click
+from botok import Text, __version__, WordTokenizer, expose_data
 from pybo import get_regex_pairs, batch_apply_regex, bo_sorted
+
+
+def prepare_folder(main=None, custom=None, overwrite=False):
+    profile = "GMD"
+    # 1. MAIN PROFILE
+    if not main:
+        # for better Windows support:
+        # https://stackoverflow.com/questions/6227590/finding-the-users-my-documents-path/6227623#6227623
+        main = Path.home() / "Documents/pybo/main"
+    else:
+        main = Path(main)
+    main.mkdir(parents=True, exist_ok=True)
+
+    if overwrite:
+        rmtree(main, ignore_errors=True)
+        main.mkdir()
+
+    try:
+        expose_data(main, profile=profile)
+    except IOError:
+        click.echo('using the existing data in "Documents/pybo/main/"')
+
+    # 2. CUSTOM PROFILE
+    if not custom:
+        custom = Path.home() / "Documents/pybo/custom"
+    else:
+        custom = Path(custom)
+    custom.mkdir(exist_ok=True)
+    for dir in [
+        "adjustment",
+        "entry_data",
+        "words_bo",
+        "words_non_inflected",
+        "words_skrt",
+    ]:
+        Path(custom / dir).mkdir(exist_ok=True)
+
+    return main, custom
 
 
 @click.group()
@@ -13,12 +52,57 @@ def cli():
 
 # Tokenize file
 @cli.command()
-@click.argument("filename", type=click.Path(exists=True))
-def tok_file(**kwargs):
-    click.echo(f'parsing {kwargs["filename"]}...')
-    t = Text(Path(kwargs["filename"]))
-    t.tokenize_words_raw_lines
-    click.echo("done")
+@click.argument("input-dir", type=click.Path(exists=True))
+@click.argument("output-dir", type=click.Path(exists=True))
+@click.option("-p", type=click.Path(exists=True), help="main-profile path")
+@click.option(
+    "-p2",
+    multiple=True,
+    type=(click.Path(exists=True), click.Path(exists=True)),
+    help="paths: main-profile, custom-profile",
+)
+@click.option("-o", "--overwrite", is_flag=True)
+def tok(**kwargs):
+    input_dir = Path(kwargs["input_dir"])
+    output_dir = Path(kwargs["output_dir"])
+    p = kwargs["p"]
+    p2 = kwargs["p2"]
+    overwrite = kwargs["overwrite"]
+
+    # prepare folder folder to receive all the botok files
+    if p and p2:
+        click.echo(
+            "Choose either -p or -p2 for the tokenizer's profiles, not both\nExiting"
+        )
+        exit(1)
+
+    if p:
+        main, custom = prepare_folder(p, overwrite=overwrite)
+        click.echo("main profile: " + p)
+    elif p2:
+        main, custom = prepare_folder(p2[0][0], p2[0][1], overwrite=overwrite)
+        click.echo("main/custom profiles: " + str(p2))
+    else:
+        main, custom = prepare_folder(overwrite=overwrite)
+        click.echo("main/custom profiles: " + str(p2))
+
+    wt = WordTokenizer(
+        tok_profile=main,
+        tok_modifs=custom,
+        tok_mode="custom",
+        adj_profile=main,
+        adj_modifs=custom,
+        adj_mode="custom",
+        conf_path=main.parent,
+    )
+
+    def tok(in_str):
+        return wt.tokenize(in_str)
+
+    for f in input_dir.glob("*.txt"):
+        print("HERE", f)
+        text = Text(f, output_dir / f.name)
+        text.custom_pipeline("basic_cleanup", tok, "words_raw_text", "plaintext")
 
 
 # Tokenize string
@@ -29,15 +113,15 @@ def tok_string(**kwargs):
     click.echo(t.tokenize_words_raw_lines)
 
 
-# rdr_2_replace_matcher
-@cli.command()
-@click.argument("infile", type=click.Path(exists=True))
-def rdr2repl(**kwargs):
-    infile = Path(kwargs["infile"])
-    outfile = infile.parent / (infile.stem + ".yaml")
-    dump = infile.read_text(encoding="utf-8-sig")
-    processed = rdr_2_replace_matcher(dump)
-    outfile.write_text(processed, encoding="utf-8-sig")
+# # rdr_2_replace_matcher
+# @cli.command()
+# @click.argument("infile", type=click.Path(exists=True))
+# def rdr2repl(**kwargs):
+#     infile = Path(kwargs["infile"])
+#     outfile = infile.parent / (infile.stem + ".yaml")
+#     dump = infile.read_text(encoding="utf-8-sig")
+#     processed = rdr_2_replace_matcher(dump)
+#     outfile.write_text(processed, encoding="utf-8-sig")
 
 
 # sort in the Tibetan order
