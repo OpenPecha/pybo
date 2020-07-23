2c34d958f7cfb3ef2d899ce4bdf0b0a5065ecff5
tenzin <ten13zin@gmail.com>
2020-07-23 18:35:17 +0530

    test: add test for extract-rules command

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,8 +1,13 @@
 from click.testing import CliRunner
 
-from pybo.cli import tok
+from pybo.cli import cli
 
 
 def test_tok():
     runner = CliRunner()
-    runner.invoke(tok, ["tests/resources/shelving/", "--tags", "pl"])
+    runner.invoke(cli, ["tok", "tests/resources/shelving/", "--tags", "pl"])
+
+
+def test_extract_rules():
+    runner = CliRunner()
+    runner.invoke(cli, ["extract-rules", "tests/resources/step2/"])
