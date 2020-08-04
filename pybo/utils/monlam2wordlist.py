55ddb5a68199ee4c6f294983d504c6c4a50eabe9
tenzin <ten13zin@gmail.com>
2020-08-04 13:05:29 +0530

    add: read csv and dump to tsv file

---
+++ pybo/utils/monlam2wordlist.py
@@ -0,0 +1,81 @@
+import csv
+import re
+
+ID = -1
+
+
+def create_word(
+    form,
+    lemma,
+    mon_pos=None,
+    mon_feature=None,
+    mon_tag=None,
+    pos=None,
+    feature=None,
+    morph=None,
+    sense_tag=None,
+    definition=None,
+    example=None,
+):
+    global ID
+    ID += 1
+    return {
+        "ID": ID,
+        "Form": form,
+        "Lemma": lemma,
+        "MonPOS": mon_pos,
+        "MonFeature": mon_feature,
+        "MonTag": mon_tag,
+        "POS": pos,
+        "Feature": feature,
+        "Morph": feature,
+        "SenseTag": sense_tag,
+        "Definition": definition,
+        "Example": example,
+    }
+
+
+def csv_loader(path):
+    with open(path, "r") as csv_file:
+        reader = csv.reader(csv_file)
+        for i, row in enumerate(reader):
+            if i == 0:
+                continue
+            yield row
+
+
+def parse_attrs(text):
+    pass
+
+
+def monlan2wordlist(rows):
+    for row in rows:
+        *_, form, result = row
+        import pdb
+
+        if form == "ལ་":
+            pdb.set_trace()
+        attrs = parse_attrs(result)
+        print(row)
+    return word_list_rows
+
+
+def dump_tsv(rows, out_path):
+    with open(out_path, "w") as csv_file:
+        writer = csv.writer(csv_file, delimiter="\t")
+        writer.writerows(rows)
+
+
+def main():
+    in_path = "../../../tibetan-nlp-dataset/monlam2020.csv"
+    out_path = "monlam2020-wordlist.tsv"
+
+    rows = csv_loader(in_path)
+    word_list = monlan2wordlist(rows)
+
+
+#    dump_tsv(word_list, out_path)
+
+
+if __name__ == "__main__":
+    exit(main())
---
+++ tests/test_monlam2word_list.py
@@ -0,0 +1,6 @@
+import pytest
+
+from pybo.utils.monlam2wordlist import csv_loader, monlan2wordlist, parse_attrs
+
+
+def test_parse_attrs():
