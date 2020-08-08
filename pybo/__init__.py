2d8f7f533e65ceb284f4218cc87437bf42342069
ngawangtrinley <ngawangtrinley@gmail.com>
2020-08-08 10:30:28 +0800

    update setup.py

--- pybo/__init__.py
+++ pybo/__init__.py
@@ -5,7 +5,6 @@ from botok import *
 
 from .corpus.parse_corrected import extract_new_entries, parse_corrected
 
-# from .utils.bo_sorted import bo_sorted
 from .pipeline.pipes import pybo_form, pybo_mod, pybo_prep
 from .utils.profile_report import profile_report
 from .utils.regex_batch_apply import batch_apply_regex, get_regex_pairs
