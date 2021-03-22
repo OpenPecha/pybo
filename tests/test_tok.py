e5768cf5a4b01ea3e6109fe938d6be0b5c630562
kaldan007 <kaldantenzin@gmail>
2021-03-22 15:22:15 +0530

    fix(cli-tok): checks input dir is directory or not and proceed accordingly

--- tests/test_tok.py
+++ tests/test_tok.py
@@ -3,6 +3,10 @@ from click.testing import CliRunner
 from pybo.cli import tok
 
 
-def test_tok():
+def test_tok_dir():
     runner = CliRunner()
-    runner.invoke(tok, ["tests/resources/shelving/", "", ""])
+    runner.invoke(tok, ["tests/resources/shelving/", "--tags", "pl"])
+
+def test_tok_file():
+    runner = CliRunner()
+    runner.invoke(tok, ["tests/resources/shelving/test_1.txt", "--tags", "p"])
